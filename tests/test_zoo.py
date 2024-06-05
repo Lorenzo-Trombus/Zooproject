eeb8f72b0332c744a4a1dba360e253872c0068d6
Nio4124 <antoniozottola41204@gmail.com>
2024-06-05 12:59:49 +0200

    nuovi test

--- tests/test_zoo.py
+++ tests/test_zoo.py
@@ -1,5 +1,48 @@
 import unittest
-#from unittest import TestCase
+from unittest import TestCase
+from src.zoo import Animal, Fence, Zoo, ZooKeeper
 
-def Test_animal_habitat(self):
-    pass
+class TestZoo(TestCase):
+# inizializza dei valori della quale abbiamo necessità in ogni funzione
+    """
+    def setUp(self) -> None:
+        self.zoo_1: Zoo = Zoo()
+        self.zookeper_1: ZooKeeper = ZooKeeper(name="Giovanni", surname="Perrella", id=1)
+        self.fence_1 : Fence = Fence(area=100, temperature= 25.0, habitat="savana")
+        self.animal : Animal = Animal(name="pippo", species="canide", age=5, height=50.0, width=1, preferred_habitat="savana")
+"""
+
+    def test_animal_dimension(self):
+        """
+Questo test controlla che animali troppo grandi non vengano aggiunti alla fence
+"""
+        zookeeper_1: ZooKeeper= ZooKeeper(name="Giovanni", surname="Perrella", id=1)
+        fence_1 : Fence = Fence(area=100, temperature=25.0, habitat="savana")
+        animal_1: Animal = Animal(name="pippo", species="canide", age=5, height=50.0, width=1, preferred_habitat="mediterrano")
+        zookeeper_1.add_animal(animal_1, fence_1)
+        result: int = len(fence_1.animals)
+        message : str = f"Error: the function add_animal should not add self.animal_1 into self.fence_1"
+
+        self.assertEqual(result, 0, message)
+
+    def test_animal_habitat(self):
+        zookeeper_1: ZooKeeper= ZooKeeper(name="Giovanni", surname="Perrella", id=1)
+        fence_1 : Fence = Fence(area=100, temperature=25.0, habitat="savana")
+        animal_1: Animal = Animal(name="pippo", species="canide", age=5, height=50.0, width=1, preferred_habitat="mediterrano")
+        zookeeper_1.add_animal(animal_1, fence_1)
+        result: int = len(fence_1.animals)
+        message : str = f"Error: the function add_animal should not add self.animal_1 into self.fence_1"
+
+        self.assertEqual(result,0,message)
+
+    def test_feed_animal(self):
+        zookeeper_1: ZooKeeper= ZooKeeper(name="Giovanni", surname="Perrella", id=1)
+        fence_1 : Fence = Fence(area=100, temperature=25.0, habitat="savana")
+        animal_1: Animal = Animal(name="pippo", species="canide", age=5, height=50.0, width=1, preferred_habitat="mediterrano")
+        zookeeper_1.feed(animal_1, fence_1)
+        result: int = (animal_1.health)
+        message : str = f"Error: the function feed is not working"
+
+        self.assertEqual(result,animal_1,message)
+
+        
